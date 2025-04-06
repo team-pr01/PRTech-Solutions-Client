593098624a76505219db46d0406aafc217257c2a
RAHUL <rahulsd380@gmail.com>
2025-04-06 17:11:26 +0600

    Setup tailwind config file

--- src/app/(commonLayout)/layout.tsx
+++ src/app/(commonLayout)/layout.tsx
@@ -1,9 +1,13 @@
+import Footer from '@/components/Shared/Footer/Footer';
+import Navbar from '@/components/Shared/Navbar/Navbar';
 import React, { ReactNode } from 'react';
 
 const MainLayout = ({children} : {children:ReactNode}) => {
     return (
         <div>
+            <Navbar/>
             {children}
+            <Footer/>
         </div>
     );
 };
