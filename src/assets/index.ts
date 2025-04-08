eeb6705922003d0248f0166b9e52ddbdf700a916
RAHUL <rahulsd380@gmail.com>
2025-04-08 12:11:11 +0600

    Fixed deployment issue

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,11 +1,11 @@
 import comingSoonPage from "../assets/images/02 - Distance Nebulae.png";
 import prtechSolutionsLogo from "../assets/images/PRTech-Solutions-Black.png";
 
-import linkedinGray from "./Icons/Social Icons/linkedin-gray.svg";
-import facebookGray from "./Icons/Social Icons/facebook-gray.svg";
-import instagramGray from "./Icons/Social Icons/instagram-gray.svg";
-import whatsappGray from "./Icons/Social Icons/whatsapp-gray.svg";
-import githubGray from "./Icons/Social Icons/github-gray.svg";
+import linkedinGray from "./icons/Social Icons/linkedin-gray.svg";
+import facebookGray from "./icons/Social Icons/facebook-gray.svg";
+import instagramGray from "./icons/Social Icons/instagram-gray.svg";
+import whatsappGray from "./icons/Social Icons/whatsapp-gray.svg";
+import githubGray from "./icons/Social Icons/github-gray.svg";
 
 export const IMAGES = {
     comingSoonPage,
