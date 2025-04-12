3ddbcfc6764c2b1627b29632daf3b32f8c481cb8
Prerna Badwane <prernabadwane@gamil.com>
2025-04-12 18:01:14 +0530

    Trusted by responsive issue resolved

--- src/components/HomePage/Hero/TrustedBy.tsx
+++ src/components/HomePage/Hero/TrustedBy.tsx
@@ -4,13 +4,13 @@ import React from "react";
 
 const TrustedBy = () => {
   return (
-    <div className="flex items-center justify-center gap-[18px]">
+    <div className="flex items-center justify-center gap-3 md:gap-[18px]">
       <Image
         src={IMAGES.trustedPeople}
         alt="Trusted By"
-        className=" h-[30px] md:h-10  w-[91px] md:w-23  object-contain"
+        className=" h-[30px] md:h-11  w-[91px] md:w-23  object-contain"
       />
-      <p className="text-white text-center font-rethink text-xs md:text-[16px] not-italic font-medium leading-none tracking-[1.6px]">
+      <p className="text-white text-center font-rethink text-[10px] sm:text-xs md:text-[16px] not-italic font-medium leading-none tracking-[1.6px]">
         Trusted already 10.6k+
       </p>
     </div>
