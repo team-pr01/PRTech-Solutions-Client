0ac327bd89d300e0883b94fdef60cc6005216561
RAHUL <rahulsd380@gmail.com>
2025-04-18 12:48:13 +0600

    Updated container

--- src/components/Reusable/Container/Container.tsx
+++ src/components/Reusable/Container/Container.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 const Container =({children} : {children:ReactNode}) => {
     return (
-        <div className="max-w-[1300px] w-full mx-auto px-5 xl:px-0">
+        <div className="max-w-[1300px] 2xl:max-w-[1450px] w-full mx-auto px-5 xl:px-0">
             {children}
         </div>
     );
