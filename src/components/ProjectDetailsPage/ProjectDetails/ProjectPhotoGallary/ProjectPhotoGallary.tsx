f12af542463dbde5326fcbbc56fe7b0a16891fb0
RAHUL <rahulsd380@gmail.com>
2025-05-02 09:22:09 +0600

    Added photo gallery using swiper

--- src/components/ProjectDetailsPage/ProjectDetails/ProjectPhotoGallary/ProjectPhotoGallary.tsx
+++ src/components/ProjectDetailsPage/ProjectDetails/ProjectPhotoGallary/ProjectPhotoGallary.tsx
@@ -21,29 +21,58 @@ const ProjectPhotoGallary = () => {
         </p>
       </div>
 
-      <Swiper
-        speed={600}
-        spaceBetween={30}
-        slidesPerView={1}
-        modules={[Navigation, Pagination]}
-        pagination={{
-          clickable: true,
-          el: ".swiper-pagination-custom",
-        }}
-        breakpoints={{
-          640: { slidesPerView: 2, spaceBetween: 25 },
-          1024: { slidesPerView: 2, spaceBetween: 30 },
-          1366: { slidesPerView: 3, spaceBetween: 30 },
-        }}
-        className="!pb-16 md:!pb-20 mt-12"
-      >
-        {[1, 2, 3, 4, 5].map((project) => (
-          <SwiperSlide key={project} className="">
-            <Image src={IMAGES.project1} alt={""} />
-          </SwiperSlide>
-        ))}
-        {/* <div className="swiper-pagination-custom text-center absolute bottom-4 left-0 right-0 cursor-pointer w-fit"></div> */}
-      </Swiper>
+      <div className="w-full relative">
+        <Swiper
+          speed={600}
+          spaceBetween={50}
+          slidesPerView={1}
+          modules={[Navigation, Pagination]}
+          navigation={{
+            prevEl: "#prevButtonDesktop, #prevButtonMobile",
+            nextEl: "#nextButtonDesktop, #nextButtonMobile",
+          }}
+          pagination={{
+            clickable: true,
+            el: ".swiper-pagination-custom",
+          }}
+          className="!pb-16 md:!pb-20"
+        >
+          {[1, 2, 3, 4, 5].map((project) => (
+            <SwiperSlide key={project}>
+              <Image src={IMAGES.project1} alt="" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        {/* Place pagination outside of <Swiper> */}
+        <div className="swiper-pagination-custom text-center absolute bottom-4 left-0 right-0 w-full z-0"></div>
+
+        <style jsx global>{`
+          .swiper-pagination-custom .swiper-pagination-bullet {
+            background-color: #fafcfd !important;
+            opacity: 0.5;
+            transition: all 0.3s ease;
+            width: 8px;
+            height: 8px;
+            margin: 0 5px !important;
+          }
+
+          .swiper-pagination-custom .swiper-pagination-bullet-active {
+            background-color: #06a0ed !important;
+            opacity: 1;
+            width: 12px;
+            height: 12px;
+            transform: translateY(-2px);
+          }
+
+          .swiper-slide {
+            height: auto;
+          }
+          .swiper-wrapper {
+            align-items: stretch;
+          }
+        `}</style>
+      </div>
     </div>
   );
 };
