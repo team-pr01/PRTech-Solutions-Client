b07a6724e957d1f01740e9d095e63580e5fa1699
RAHUL <rahulsd380@gmail.com>
2025-05-02 09:39:19 +0600

    Added progress bar in photo gallery slider

--- src/components/ProjectDetailsPage/ProjectDetails/ProjectPhotoGallary/ProjectPhotoGallary.tsx
+++ src/components/ProjectDetailsPage/ProjectDetails/ProjectPhotoGallary/ProjectPhotoGallary.tsx
@@ -1,13 +1,29 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import { Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import Image from "next/image";
 import { IMAGES } from "@/assets";
+import { useRef, useState } from "react";
 
 const ProjectPhotoGallary = () => {
+  const [progress, setProgress] = useState(0); // 0 to 100
+  // The ref is not strictly needed for this logic but doesn't hurt
+  const progressRef = useRef<HTMLDivElement | null>(null);
+
+  // --- Corrected Handler ---
+  const handleAutoplayTimeLeft = (
+    swiper: any,
+    time: number,
+    progressRatio: number
+  ) => {
+    // progressRatio goes from 0 (start of delay) to 1 (end of delay)
+    // We want the bar width to go from 0% to 100%
+    setProgress((1 - progressRatio) * 100);
+  };
   return (
     <div className="flex flex-col gap-6 mt-[50px]">
       <div>
@@ -22,31 +38,49 @@ const ProjectPhotoGallary = () => {
       </div>
 
       <div className="w-full relative">
+        {/* Progress Bar Container */}
+        <div className="bg-neutral-70/70 h-0.5 relative rounded-xl overflow-hidden mb-4">
+          <div
+            ref={progressRef}
+            className="bg-primary-20 h-full rounded-xl"
+            style={{ width: `${progress}%` }}
+          ></div>
+        </div>
+
+        {/* Swiper Component */}
         <Swiper
           speed={600}
           spaceBetween={50}
           slidesPerView={1}
-          modules={[Navigation, Pagination]}
-          navigation={{
-            prevEl: "#prevButtonDesktop, #prevButtonMobile",
-            nextEl: "#nextButtonDesktop, #nextButtonMobile",
+          modules={[Navigation, Pagination, Autoplay]}
+          autoplay={{
+            delay: 3000,
+            disableOnInteraction: false,
           }}
+          onAutoplayTimeLeft={handleAutoplayTimeLeft}
           pagination={{
             clickable: true,
             el: ".swiper-pagination-custom",
           }}
-          className="!pb-16 md:!pb-20"
+          className="!pb-12"
         >
           {[1, 2, 3, 4, 5].map((project) => (
             <SwiperSlide key={project}>
-              <Image src={IMAGES.project1} alt="" />
+              <Image
+                src={IMAGES.project1}
+                alt={`Project ${project}`}
+                width={800}
+                height={450}
+                style={{ display: "block", width: "100%", height: "auto" }}
+              />
             </SwiperSlide>
           ))}
         </Swiper>
 
-        {/* Place pagination outside of <Swiper> */}
+        {/* Pagination dots */}
         <div className="swiper-pagination-custom text-center absolute bottom-4 left-0 right-0 w-full z-0"></div>
 
+          {/* Styles for dots */}
         <style jsx global>{`
           .swiper-pagination-custom .swiper-pagination-bullet {
             background-color: #fafcfd !important;
