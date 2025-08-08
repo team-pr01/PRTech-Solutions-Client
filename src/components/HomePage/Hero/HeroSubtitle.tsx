7d6cdd689f40a0ef6fd972c47ab479e923767a38
rahulsd380 <rahulsd380@gmail.com>
2025-08-08 23:29:22 +0600

    removed projects navlinks

--- src/components/HomePage/Hero/HeroSubtitle.tsx
+++ src/components/HomePage/Hero/HeroSubtitle.tsx
@@ -8,6 +8,7 @@ const HeroSubtitle = () => {
       <div className="bg-transparent inset-0 z-[2] relative w-full sm:w-fit rounded-full gap-4 p-0.5">
         <div className="relative z-[2] rounded-full bg-whites-50 py-[10px] px-4 md:px-[30px]">
           <div className="absolute top-0 left-0 right-0 bottom-0 w-full bg-primary-30 opacity-10 h-full rounded-full"></div>
+          {/* Content */}
           <div className="flex flex-row items-center justify-center gap-2 md:gap-[9px] w-full">
             <Image
               src={ICONS.stars}
@@ -23,6 +24,7 @@ const HeroSubtitle = () => {
               className="size-[15px] md:size-5 animate-pulse"
             />
           </div>
+          {/* Content end */}
         </div>
       </div>
     </div>
--- src/components/Shared/Navbar/navLinks.ts
+++ src/components/Shared/Navbar/navLinks.ts
@@ -1,6 +1,6 @@
 export const navLinks = [
     { name: 'Home', href: '/' },
-    { name: 'Projects', href: '/our-projects' },
+    // { name: 'Projects', href: '/our-projects' },
     { name: 'About Us', href: '/about-us' },
     { name: 'Blogs', href: '/blogs' },
     { name: 'Contact Us', href: '/contact-us' },
