8d3c621fdf92fe6fd353c509051e9dfd59ed1496
prernabadwane <prernabadwane@gmail.com>
2025-08-11 21:25:08 +0530

    chat bot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,24 +15,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-       <Script id="tawkto-script" strategy="afterInteractive">
-        {`
-          var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-          (function(){
-            var s1 = document.createElement("script"),
-                s0 = document.getElementsByTagName("script")[0];
-            s1.async = true;
-            s1.src = 'https://embed.tawk.to/68977d3c6ec67e192b924fcb/1j27s2dch';
-            s1.charset = 'UTF-8';
-            s1.setAttribute('crossorigin', '*');
-            s0.parentNode.insertBefore(s1, s0);
-          })();
-        `}
-      </Script>
-      <body
-        className={`antialiased bg-secondary-50`}
-      >
+      <body className="antialiased bg-secondary-50">
         <ClientProvider>{children}</ClientProvider>
+
+        {/* Tawk.to Script */}
+        <Script id="tawkto-script" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+              s1.async=true;
+              s1.src='https://embed.tawk.to/68977d3c6ec67e192b924fcb/1j29gpji7';
+              s1.charset='UTF-8';
+              s1.setAttribute('crossorigin','*');
+              s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
