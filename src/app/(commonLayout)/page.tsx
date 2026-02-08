fd21b6a8343446b5f7f8ae689d1c6b7036be075c
rahulsd380 <rahulsd380@gmail.com>
2026-02-08 20:40:14 +0600

    Added dynamic data for ind in homepage and about us page

--- src/app/(commonLayout)/page.tsx
+++ src/app/(commonLayout)/page.tsx
@@ -1,10 +1,8 @@
 import Banner from "@/components/HomePage/Banner/Banner";
 import Hero from "@/components/HomePage/Hero/Hero";
-import InAction from "@/components/HomePage/InAction/InAction";
 import OurProgressBar from "@/components/HomePage/OurProgressBar/OurProgressBar";
 import OurProjects from "@/components/HomePage/OurProjects/OurProjects";
 import OurServices from "@/components/HomePage/OurServices/OurServices";
-import OurTechStack from "@/components/HomePage/OurTechStack/OurTechStack";
 import Testimonials from "@/components/HomePage/Testimonials/Testimonials";
 import AboutPRTech from "@/components/Shared/AboutPRTech/AboutPRTech";
 // import ProjectTimeline from "@/components/ProjectTimeline/ProjectTimeline";
@@ -16,8 +14,8 @@ export default function Home() {
       <OurServices/>
       <AboutPRTech/>
       <OurProjects/>
-      <InAction/> 
-      <OurTechStack/>
+      {/* <InAction/>  */}
+      {/* <OurTechStack/> */}
       <OurProgressBar/>
       <Testimonials/>
       <Banner/>
---
+++ src/data/homepage.data.ts
@@ -0,0 +1,141 @@
+import { ICONS } from "@/assets";
+
+export const homePageData = {
+  ind: {
+    hero: {
+      subTitle: "Establish Business Online And Test Potential",
+      styledTitle: "We craft digital",
+      title: "experiences that inspire",
+      description:
+        "Websites and apps that look sharp, work fast, and help your brand grow.",
+    },
+
+    aboutUs: {
+      missionAndVision: [
+        {
+          icon: ICONS.missionGif,
+          title: "Our Mission",
+          description:
+            "We help businesses grow through clean, scalable, and user-focused digital solutions — built with care and purpose.",
+        },
+        {
+          icon: ICONS.visionGif,
+          title: "Our Vision",
+          description:
+            "To become a go-to partner for brands seeking innovative, high-performing websites and apps that make an impact.",
+        },
+        {
+          icon: ICONS.disclaimerGif,
+          title: "Disclaimer",
+          description:
+            "PRtech Solutions delivers digital services tailored to client needs; results may vary based on requirements, scope, and usage.",
+        },
+      ],
+    },
+
+    cta: {
+      title: "Ready to transform your digital presence?",
+      description:
+        "Let’s discuss how we can help you achieve your business goals with our expertise — from tailored strategies to innovative solutions, we’re here to drive your success every step of the way.",
+      subtitleAnimatedPointers: [
+        "build websites",
+        "develop apps",
+        "create digital solutions",
+        "design user experiences",
+        "boost your brand",
+      ],
+    },
+  },
+
+  bn: {
+    hero: {
+      subTitle: "Establish Business Online And Test Potential",
+      styledTitle: "We craft digital",
+      title: "experiences that inspire",
+      description:
+        "Websites and apps that look sharp, work fast, and help your brand grow.",
+    },
+
+    aboutUs: {
+      missionAndVision: [
+        {
+          icon: ICONS.missionGif,
+          title: "Our Mission",
+          description:
+            "We help businesses grow through clean, scalable, and user-focused digital solutions — built with care and purpose.",
+        },
+        {
+          icon: ICONS.visionGif,
+          title: "Our Vision",
+          description:
+            "To become a go-to partner for brands seeking innovative, high-performing websites and apps that make an impact.",
+        },
+        {
+          icon: ICONS.disclaimerGif,
+          title: "Disclaimer",
+          description:
+            "PRtech Solutions delivers digital services tailored to client needs; results may vary based on requirements, scope, and usage.",
+        },
+      ],
+    },
+
+    cta: {
+      title: "Ready to transform your digital presence?",
+      description:
+        "Let’s discuss how we can help you achieve your business goals with our expertise — from tailored strategies to innovative solutions, we’re here to drive your success every step of the way.",
+      subtitleAnimatedPointers: [
+        "build websites",
+        "develop apps",
+        "create digital solutions",
+        "design user experiences",
+        "boost your brand",
+      ],
+    },
+  },
+
+  uae: {
+    hero: {
+      subTitle: "Establish Business Online And Test Potential",
+      styledTitle: "We craft digital",
+      title: "experiences that inspire",
+      description:
+        "Websites and apps that look sharp, work fast, and help your brand grow.",
+    },
+
+    aboutUs: {
+      missionAndVision: [
+        {
+          icon: ICONS.missionGif,
+          title: "Our Mission",
+          description:
+            "We help businesses grow through clean, scalable, and user-focused digital solutions — built with care and purpose.",
+        },
+        {
+          icon: ICONS.visionGif,
+          title: "Our Vision",
+          description:
+            "To become a go-to partner for brands seeking innovative, high-performing websites and apps that make an impact.",
+        },
+        {
+          icon: ICONS.disclaimerGif,
+          title: "Disclaimer",
+          description:
+            "PRtech Solutions delivers digital services tailored to client needs; results may vary based on requirements, scope, and usage.",
+        },
+      ],
+    },
+
+    cta: {
+      title: "Ready to transform your digital presence?",
+      description:
+        "Let’s discuss how we can help you achieve your business goals with our expertise — from tailored strategies to innovative solutions, we’re here to drive your success every step of the way.",
+      subtitleAnimatedPointers: [
+        "build websites",
+        "develop apps",
+        "create digital solutions",
+        "design user experiences",
+        "boost your brand",
+      ],
+    },
+  },
+};
